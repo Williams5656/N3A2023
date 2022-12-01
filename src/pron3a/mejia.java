e64e1f3ec8f0f10a9ab7efd0adcac0b0d888130d
PC01 <PC01@DESKTOP-2N6FAOT>
2022-12-02 00:42:03 +0100

    primera clase

---
+++ src/pron3a/mejia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pron3a;
+
+/**
+ *
+ * @author PC01
+ */
+public class mejia {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Danny Mejia");
+        System.out.println("---------------------");
+    }
+}
