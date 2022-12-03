c4d606c2e55e6196d813828eb528e3bb2349e1d1
User <User@LAPTOP-E9B6PQCO.mshome.net>
2022-12-02 20:20:06 -0500

    Clase Producto n

---
+++ src/SistemaProductos/Productos_class.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SistemaProductos;
+
+/**
+ *
+ * @author User
+ */
+public class Productos_class {
+    int codigo;
+    String nombre;
+    String tipo;
+    String proveedor;
+    String calidad;
+    double precio;
+
+    public Productos_class(int codigo, String nombre, String tipo, String proveedor, String calidad, double precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.proveedor = proveedor;
+        this.calidad = calidad;
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public String getCalidad() {
+        return calidad;
+    }
+
+    public void setCalidad(String calidad) {
+        this.calidad = calidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
