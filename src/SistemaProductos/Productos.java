7f67b18dd7d4f35ab441fefeba438109a9d2d7c0
HP GAMER <HP GAMER@DESKTOP-5OVN317.mshome.net>
2022-12-02 20:57:21 -0500

    prueba2 creacion ventana Productos inf. mas inf.

--- src/SistemaProductos/Productos.java
+++ src/SistemaProductos/Productos.java
@@ -36,16 +36,17 @@ public class Productos extends javax.swing.JInternalFrame {
         btn_guardar = new javax.swing.JButton();
         btn_editar = new javax.swing.JButton();
         btn_eliminar = new javax.swing.JButton();
-        id_tipo = new javax.swing.JComboBox<>();
         id_codigo = new javax.swing.JTextField();
         id_nombre = new javax.swing.JTextField();
         id_calidad = new javax.swing.JTextField();
         id_precio = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        tabla = new javax.swing.JTable();
         btn_limpiar = new javax.swing.JButton();
         id_proveedor = new javax.swing.JTextField();
         proveedor = new javax.swing.JLabel();
+        txt_tipo = new javax.swing.JTextField();
+        btn_salir = new javax.swing.JButton();
 
         productos.setFont(new java.awt.Font("Times New Roman", 1, 48)); // NOI18N
         productos.setText("PRODUCTOS");
@@ -76,15 +77,13 @@ public class Productos extends javax.swing.JInternalFrame {
 
         btn_eliminar.setText("ELIMINAR");
 
-        id_tipo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-
         id_codigo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 id_codigoActionPerformed(evt);
             }
         });
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tabla.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -95,12 +94,14 @@ public class Productos extends javax.swing.JInternalFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(tabla);
 
         btn_limpiar.setText("LIMPIAR");
 
         proveedor.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
         proveedor.setText("PROVEEDOR");
+
+        btn_salir.setText("SALIR");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -121,25 +122,30 @@ public class Productos extends javax.swing.JInternalFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(id_nombre)
                             .addComponent(id_codigo)
-                            .addComponent(id_tipo, 0, 230, Short.MAX_VALUE)
                             .addComponent(id_calidad)
                             .addComponent(id_precio)
-                            .addComponent(id_proveedor))
+                            .addComponent(id_proveedor)
+                            .addComponent(txt_tipo, javax.swing.GroupLayout.DEFAULT_SIZE, 230, Short.MAX_VALUE))
                         .addGap(52, 52, 52)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(305, 305, 305)
                         .addComponent(productos)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(167, 167, 167)
-                .addComponent(btn_guardar)
-                .addGap(120, 120, 120)
-                .addComponent(btn_editar)
-                .addGap(112, 112, 112)
-                .addComponent(btn_eliminar)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 142, Short.MAX_VALUE)
-                .addComponent(btn_limpiar)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btn_salir))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(167, 167, 167)
+                        .addComponent(btn_guardar)
+                        .addGap(120, 120, 120)
+                        .addComponent(btn_editar)
+                        .addGap(112, 112, 112)
+                        .addComponent(btn_eliminar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 142, Short.MAX_VALUE)
+                        .addComponent(btn_limpiar)))
                 .addGap(78, 78, 78))
         );
         layout.setVerticalGroup(
@@ -160,7 +166,7 @@ public class Productos extends javax.swing.JInternalFrame {
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(tipo)
-                            .addComponent(id_tipo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(txt_tipo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(calidad)
@@ -173,9 +179,9 @@ public class Productos extends javax.swing.JInternalFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(id_proveedor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(proveedor)))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(btn_guardar)
                         .addGap(97, 97, 97))
@@ -184,7 +190,9 @@ public class Productos extends javax.swing.JInternalFrame {
                             .addComponent(btn_eliminar)
                             .addComponent(btn_editar)
                             .addComponent(btn_limpiar))
-                        .addContainerGap())))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btn_salir)
+                        .addGap(31, 31, 31))))
         );
 
         pack();
@@ -205,6 +213,7 @@ public class Productos extends javax.swing.JInternalFrame {
     private javax.swing.JButton btn_eliminar;
     private javax.swing.JButton btn_guardar;
     private javax.swing.JButton btn_limpiar;
+    private javax.swing.JButton btn_salir;
     private javax.swing.JLabel calidad;
     private javax.swing.JLabel codigo;
     private javax.swing.JTextField id_calidad;
@@ -212,13 +221,13 @@ public class Productos extends javax.swing.JInternalFrame {
     private javax.swing.JTextField id_nombre;
     private javax.swing.JTextField id_precio;
     private javax.swing.JTextField id_proveedor;
-    private javax.swing.JComboBox<String> id_tipo;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     private javax.swing.JLabel nombre;
     private javax.swing.JLabel precio;
     private javax.swing.JLabel productos;
     private javax.swing.JLabel proveedor;
+    private javax.swing.JTable tabla;
     private javax.swing.JLabel tipo;
+    private javax.swing.JTextField txt_tipo;
     // End of variables declaration//GEN-END:variables
 }
